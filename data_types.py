2941e7ed224bf5f4490ea8e8ea254a50042a85fd
Nurdaulet <n_berik@kbtu.kz>
2026-01-29 17:28:11 +0500

    Add Practice1 Python basics examples

---
+++ data_types.py
@@ -0,0 +1,21 @@
+a = 7
+b = 7.5
+c = "Nurdaulet"
+d = True
+e = complex(1j)
+f = list(("apple", "banana", "cherry"))
+j = tuple(("apple", "banana", "cherry"))
+i = range(6)
+h = dict(name="John", age=36)
+o = set(("apple", "banana", "cherry"))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(j))
+print(type(i))
+print(type(h))
+print(type(o))
+
