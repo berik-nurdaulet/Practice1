e9e908429450bfb07c1dd8621df9266b228ff016
Nurdaulet <n_berik@kbtu.kz>
2026-02-03 16:06:49 +0500

    NEW

---
+++ pyhton-basics/hello_world.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+print("Nurdaulet")
+print("KBTU is the best")
+print("I'm from Kazakhstan")
+print("Salem, Alem")
+
---
+++ pyhton-basics/variables.py
@@ -0,0 +1,29 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+y = "Sally" # x is now of type str
+print(x)
+print(y)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
